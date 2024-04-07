5c8f7b823befeab28963b7d941248d7c814838a6
JatinDhamija816 <jdhamija816@gmail.com>
2024-04-07 13:18:26 +0530

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,20 +12,20 @@ const Main = () => {
             <Header />
             <div className="w-11/12 m-auto border border-white rounded-lg md:max-w-full">
                 <Filter />
-                <div className="flex justify-evenly mx-5 md:max-w-min">
-                    <div className="m-1 p-1 w-1/5 md:min-w-1/5">
+                <div className="flex justify-evenly mx-5 md:min-w-">
+                    <div className="m-1 p-1 w-1/5 md:min-w-fit">
                         <Pending />
                     </div>
-                    <div className="m-1 p-1 w-1/5 md:min-w-1/5">
+                    <div className="m-1 p-1 w-1/5 md:min-w-fit">
                         <Progress />
                     </div>
-                    <div className="m-1 p-1 w-1/5 md:min-w-1/5">
+                    <div className="m-1 p-1 w-1/5 md:min-w-fit">
                         <Completed />
                     </div>
-                    <div className="m-1 p-1 w-1/5 md:min-w-1/5">
+                    <div className="m-1 p-1 w-1/5 md:min-w-fit">
                         <Deployed />
                     </div>
-                    <div className="m-1 p-1 w-1/5 md:min-w-1/5">
+                    <div className="m-1 p-1 w-1/5 md:min-w-fit">
                         <Deffered />
                     </div>
                 </div>
